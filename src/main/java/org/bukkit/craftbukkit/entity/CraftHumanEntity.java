d644ee958a7d4c9dbc1aeb94086f5263177f3306
Dinnerbone <dinnerbone@dinnerbone.com>
2011-07-02 23:31:37 +0100

    Made CraftHumanEntity implement Permissible (aka: Permissions work!)

--- src/main/java/org/bukkit/craftbukkit/entity/CraftHumanEntity.java
+++ src/main/java/org/bukkit/craftbukkit/entity/CraftHumanEntity.java
@@ -1,15 +1,25 @@
 
 package org.bukkit.craftbukkit.entity;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import net.minecraft.server.EntityHuman;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.craftbukkit.inventory.CraftInventoryPlayer;
 import org.bukkit.craftbukkit.CraftServer;
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionAttachment;
+import org.bukkit.permissions.PermissionRemovedExecutor;
 
 public class CraftHumanEntity extends CraftLivingEntity implements HumanEntity {
     private CraftInventoryPlayer inventory;
+    private final List<PermissionAttachment> attachments = new LinkedList<PermissionAttachment>();
+    private final Map<String, Boolean> permissions = new HashMap<String, Boolean>();
+    private boolean dirtyPermissions = true;
 
     public CraftHumanEntity(final CraftServer server, final EntityHuman entity) {
         super(server, entity);
@@ -54,5 +64,94 @@ public class CraftHumanEntity extends CraftLivingEntity implements HumanEntity {
 
     public int getSleepTicks() {
         return getHandle().sleepTicks;
+    }
+
+    public boolean isPermissionSet(String name) {
+        calculatePermissions();
+
+        return permissions.get(name.toLowerCase());
+    }
+
+    public boolean isPermissionSet(Permission perm) {
+        return isPermissionSet(perm.getName());
+    }
+
+    public boolean hasPermission(String inName) {
+        calculatePermissions();
+
+        String name = inName.toLowerCase();
+
+        if (!isPermissionSet(name)) {
+            return permissions.get(name);
+        } else {
+            Permission perm = getServer().getPluginManager().getPermission(name);
+
+            if (perm != null) {
+                return perm.getDefault();
+            } else {
+                return false;
+            }
+        }
+    }
+
+    public boolean hasPermission(Permission perm) {
+        calculatePermissions();
+
+        String name = perm.getName().toLowerCase();
+
+        if (!isPermissionSet(name)) {
+            return permissions.get(name);
+        } else if (perm != null) {
+            return perm.getDefault();
+        } else {
+            return false;
+        }
+    }
+
+    public PermissionAttachment addAttachment(String name, boolean value) {
+        PermissionAttachment result = addAttachment();
+        result.setPermission(name, value);
+
+        recalculatePermissions();
+
+        return result;
+    }
+
+    public PermissionAttachment addAttachment() {
+        PermissionAttachment result = new PermissionAttachment(this);
+
+        attachments.add(result);
+        recalculatePermissions();
+
+        return result;
+    }
+
+    public void removeAttachment(PermissionAttachment attachment) {
+        if (attachments.contains(attachment)) {
+            attachments.remove(attachment);
+            PermissionRemovedExecutor ex = attachment.getRemovalCallback();
+            
+            if (ex != null) {
+                ex.attachmentRemoved(attachment);
+            }
+        } else {
+            throw new IllegalArgumentException("Given attachment is not part of Permissible object " + this);
+        }
+    }
+
+    public void recalculatePermissions() {
+        dirtyPermissions = true;
+    }
+
+    private synchronized void calculatePermissions() {
+        if (dirtyPermissions) {
+            permissions.clear();
+
+            for (PermissionAttachment attachment : attachments) {
+                permissions.putAll(attachment.getPermissions());
+            }
+
+            dirtyPermissions = false;
+        }
     }
 }
